e3600e9178f4f457b4d8a1478e2c8a2cd724e30b
alison1313 <yunji_18@naver.com>
2022-10-18 19:04:16 +0900

    이윤지 회원탈퇴

--- src/main/java/project/vinyl/controller/MemberController.java
+++ src/main/java/project/vinyl/controller/MemberController.java
@@ -3,15 +3,21 @@ package project.vinyl.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.vinyl.constant.SessionConst;
 import project.vinyl.dto.AddMemberDto;
+import project.vinyl.dto.LoginFormDto;
 import project.vinyl.entity.Member;
 import project.vinyl.service.MemberService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @Slf4j
@@ -27,12 +33,54 @@ public class MemberController {
     }
 
     @PostMapping("/add")
-    public String save(@Valid @ModelAttribute AddMemberDto addMemberDto, BindingResult bindingResult) {
+    public String save(@Valid @ModelAttribute AddMemberDto addMemberDto, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "members/addMemberForm";
         }
-        Member member = Member.createMember(addMemberDto);
-        memberService.join(member);
+        try {
+            Member member = Member.createMember(addMemberDto);
+            memberService.join(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "members/addMemberForm";
+        }
+
+        return "redirect:/home";
+    }
+    @GetMapping("/delete")
+    public String delete(@ModelAttribute("loginFormDto") LoginFormDto loginFormDto) {
+        return "members/deleteMemberForm";
+    }
+
+    @Transactional
+    @PostMapping("/delete")
+    public String delete(@Valid @ModelAttribute LoginFormDto form, BindingResult bindingResult, HttpServletRequest request){
+        if (bindingResult.hasErrors()) {
+            log.info("return");
+            return "members/deleteMemberForm";
+        }
+
+        HttpSession session = request.getSession();
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        Member deleteMember = memberService.check(form.getLoginId(), form.getPassword());
+
+        if (deleteMember == null) {
+            bindingResult.reject("loginFail", "아이디 혹은 비밀번호를 정확하게 입력하세요.");
+            return "members/deleteMemberForm";
+        }
+
+        if (loginMember.getLoginId() == deleteMember.getLoginId()) {
+            memberService.delete(deleteMember);
+            if (session != null) {
+                session.invalidate();
+            }
+        }
+        else {
+            bindingResult.reject("checkFail", "현재 로그인한 회원이 아닙니다.");
+            return "members/deleteMemberForm";
+        }
+
         return "redirect:/home";
     }
 }
--- src/main/java/project/vinyl/service/MemberService.java
+++ src/main/java/project/vinyl/service/MemberService.java
@@ -5,12 +5,32 @@ import org.springframework.stereotype.Service;
 import project.vinyl.entity.Member;
 import project.vinyl.repository.MemberRepository;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member) {
+        validateDuplicateMember(member);
         return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> findMember = memberRepository.findByLoginId(member.getLoginId());
+        if (findMember.isPresent()) {
+            throw new IllegalStateException("이미 존재하는 ID입니다.");
+        }
+    }
+
+    public void delete(Member member) {
+        memberRepository.delete(member);
+    }
+
+    public Member check(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
     }
 }
