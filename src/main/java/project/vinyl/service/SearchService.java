331747570e02792088d14b7fe063b433bf6f9a54
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-11-12 20:58:48 +0900

    저장용

--- src/main/java/project/vinyl/service/SearchService.java
+++ src/main/java/project/vinyl/service/SearchService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import project.vinyl.dto.SearchDto;
@@ -199,5 +200,49 @@ public class SearchService {
         }
         return list;
     }
+
+    /*public  List<ComparePriceDto> yes24Vinyl(String keyword){
+        List<ComparePriceDto> comparePriceDtoList = new ArrayList<>();
+        if ( keyword != null){
+            keyword = keyword + " lp";
+            int page=1;
+            String seoul = "http://www.yes24.com/Product/Search?domain=MUSIC&query="+keyword;
+            Connection conn = Jsoup.connect(seoul);
+            try{
+                Document document = conn.get();
+
+                Elements select = document.select(".sGoodsSecArea > div.noData");
+                if(select.size()==0){
+                    while(true){
+                        Elements elements = document.select("#yesSchList > li > div.itemUnit");
+                        if(elements.size()==0 || comparePriceDtoList.size()>10)break;
+                        for (Element element : elements) {
+                            try {
+                                if(comparePriceDtoList.size()>10)break;
+                                String link = "http://www.yes24.com"+element.select("a").get(0).attr("href");
+                                String img = element.select("img").get(0).attr("data-original");
+                                String name = element.select("a.gd_name").get(0).text();
+                                String price = element.select("em.yes_b").get(0).text();
+                                boolean available = element.select("span.soldOut").size() == 0 ? true : false;
+                                ComparePriceDto comparePriceDto = new ComparePriceDto(img, link, name, price, available);
+                                comparePriceDtoList.add(comparePriceDto);
+                            }catch (IndexOutOfBoundsException e){
+                                continue;
+                            }
+                        }
+                        page+=1;
+                        String nextPage = "http://www.yes24.com/Product/Search?domain=MUSIC&query=" + keyword +
+                                "&page="+Integer.toString(page);
+                        Connection nextCon = Jsoup.connect(nextPage);
+                        document=nextCon.get();
+                    }
+                    return comparePriceDtoList;
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return comparePriceDtoList;
+    }*/
 
 }
