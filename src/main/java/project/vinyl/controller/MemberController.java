2a566d11435a788e5bf5f5804a080ec8f43241c8
alison1313 <yunji_18@naver.com>
2022-11-23 00:44:24 +0900

    비밀번호 암호화

--- src/main/java/project/vinyl/controller/MemberController.java
+++ src/main/java/project/vinyl/controller/MemberController.java
@@ -2,13 +2,11 @@ package project.vinyl.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import project.vinyl.constant.SessionConst;
 import project.vinyl.dto.AddMemberDto;
 import project.vinyl.dto.LoginFormDto;
@@ -38,8 +36,7 @@ public class MemberController {
             return "members/addMemberForm";
         }
         try {
-            Member member = Member.createMember(addMemberDto);
-            memberService.join(member);
+            memberService.join(addMemberDto);
         } catch (IllegalStateException e) {
             model.addAttribute("errorMessage", e.getMessage());
             return "members/addMemberForm";
--- src/main/java/project/vinyl/service/MemberService.java
+++ src/main/java/project/vinyl/service/MemberService.java
@@ -1,7 +1,10 @@
 package project.vinyl.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import project.vinyl.dto.AddMemberDto;
 import project.vinyl.entity.Member;
 import project.vinyl.entity.TotalLedger;
 import project.vinyl.repository.MemberRepository;
@@ -9,22 +12,29 @@ import project.vinyl.repository.TotalLedgerRepository;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final TotalLedgerRepository totalLedgerRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public Member join(Member member) {
-        validateDuplicateMember(member);
+    public Member join(AddMemberDto addMemberDto) {
+        Member member = Member.createMember(addMemberDto);
+
+        validateDuplicateMember(member.getLoginId());
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+
         memberRepository.save(member);
         TotalLedger totalLedger = new TotalLedger(member);
         totalLedgerRepository.save(totalLedger);
+
         return member;
     }
 
-    private void validateDuplicateMember(Member member) {
-        Optional<Member> findMember = memberRepository.findByLoginId(member.getLoginId());
+    public void validateDuplicateMember(String loginId) {
+        Optional<Member> findMember = memberRepository.findByLoginId(loginId);
         if (findMember.isPresent()) {
             throw new IllegalStateException("이미 존재하는 ID입니다.");
         }
